8549e953467fbce09509eeb996defb38fb09fde0
cosmihai <cos_mihai23@yahoo.com>
2019-11-24 22:36:59 +0100

    tests for restaurants and customers login done

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -6,43 +6,20 @@ const request = require("supertest");
 let server;
 
 describe("/api/auth", () => {
-
+  let payload;
+  let url;
   beforeEach(() => {
     server = require('../../app');
   });
   afterEach(() => {
     server.close();
   });
-  function exec(url, body) {
+  function exec(endPoint, body) {
     return request(server)
-    .post(url)
+    .post(endPoint)
     .send(body)
   };
-  describe("POST /bartenders/login", () => {
-    const url = '/api/auth/bartenders/login'
-    let bartender;
-    let payload;
-    beforeEach(async () => {
-      bartender = {
-        username: "bartenderTest1",
-        email: "bartenderTest1@gmail.com",
-        password: bcrypt.hashSync('123456', bcrypt.genSaltSync(10)),
-        personalInfo: {
-          firstName: "firstNameTest1",
-          lastName: "lastNameTest1",
-          phone: "654321",
-          description: "this is test description for bartenderTest1"
-        }
-      };
-      payload = {
-        email: "bartenderTest1@gmail.com",
-        password: "123456"
-      };
-      await Bartender.create(bartender);
-    });
-    afterEach(async () => {
-      await Bartender.deleteMany({});
-    })
+  function runTests() {
     it("Should return 400 if email is missing", async () => {
       payload.email = '';
       const res = await exec(url, payload);
@@ -68,10 +45,88 @@ describe("/api/auth", () => {
       expect(res.body.message).toMatch('Password or email incorrect');
     });
     it("Should return 200 if password and email are correct", async () => {
-      payload.password = '123456'
       const res = await exec(url, payload);
       expect(res.status).toBe(200);
       expect(res.body.token).not.toBe(null);
     });
+  }
+
+  describe("POST /bartenders/login", () => {
+    let bartender;
+    beforeEach(async () => {
+      url = '/api/auth/bartenders/login';
+      bartender = {
+        username: "bartenderTest1",
+        email: "bartenderTest1@gmail.com",
+        password: bcrypt.hashSync('123456', bcrypt.genSaltSync(10)),
+        personalInfo: {
+          firstName: "firstNameTest1",
+          lastName: "lastNameTest1",
+          phone: "654321",
+          description: "this is test description for bartenderTest1"
+        }
+      };
+      payload = {
+        email: "bartenderTest1@gmail.com",
+        password: "123456"
+      };
+      await Bartender.create(bartender);
+    });
+    afterEach(async () => {
+      await Bartender.deleteMany({});
+    })
+    runTests();
+  });
+
+  describe("POST /restaurants/login", () => {
+    let restaurant;
+    beforeEach(async () => {
+      url = '/api/auth/restaurants/login';
+      restaurant = {
+        name: 'restaurantTest1',
+        email: 'restaurantTest1@gmail.com',
+        password: bcrypt.hashSync('123456', bcrypt.genSaltSync(10)),
+        address: {
+          street: "main st.",
+          number: '00',
+          city: 'City',
+          country: 'Country'
+        },
+        phone: '000000',
+        description: 'test description for restaurantTest1',
+        capacity: 100,
+        cuisine: 'test cuisine'
+      };
+      payload = {
+        email: 'restaurantTest1@gmail.com',
+        password: '123456'
+      };
+      await Restaurant.create(restaurant);
+    });
+    afterEach(async () => {
+      await Restaurant.deleteMany({});
+    });
+    runTests();
+  });
+
+  describe("POST /api/customers/login", () => {
+    let customer;
+    beforeEach(async () => {
+      url = '/api/auth/customers/login';
+      customer = {
+        username: 'customerTest1',
+        email: 'customerTest1@gmail.com',
+        password: bcrypt.hashSync('123456', bcrypt.genSaltSync(10))
+      };
+      payload = {
+        email: 'customerTest1@gmail.com',
+        password: '123456'
+      };
+      await Customer.create(customer);
+    });
+    afterEach(async () => {
+      await Customer.deleteMany({});
+    });
+    runTests();
   });
 });
