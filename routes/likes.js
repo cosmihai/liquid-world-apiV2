6c4ead5ac22303a256185b9549e4b0ef8c95f8f7
cosmihai <cos_mihai23@yahoo.com>
2019-12-15 23:16:36 +0100

    finished tests for /api/likes

--- routes/likes.js
+++ routes/likes.js
@@ -15,17 +15,17 @@ router.get('/', async (req, res) => {
 //give like to a cocktail
 router.post('/', auth, async (req, res) => {
   //authorize
-  if(req.user.role != 'customer') return res.status(401).send(`Only customers can give like to a cocktail`);
+  if(req.user.role != 'customer') return res.status(401).send({message: `Only customers can give like to a cocktail`});
   //set the customer
   const customer = await Customer.findById(req.user._id);
-  if(!customer) return res.status(400).send(`No customer user with this id`);
+  if(!customer) return res.status(400).send({message: `No customer user with this id`});
   //validate the body of the request
   const error = validateLike(req.body);
-  if(error) return res.status(400).send(error.details[0].message);
+  if(error) return res.status(400).send(error.details[0]);
   const cocktail = await Cocktail.findById(req.body.cocktailId);
-  if(!cocktail) return res.status(400).send(`No cocktail with this id`);
+  if(!cocktail) return res.status(404).send({message: `No cocktail with this id`});
   //check if already is in favCocktail list
-  if(customer.favCocktails.indexOf(cocktail._id) > -1) return res.status(400).send(`You already liked this cocktail`);
+  if(customer.favCocktails.indexOf(cocktail._id) > -1) return res.status(400).send({message: `You already like this cocktail`});
   //set the like
   const like = new Like({
     customerId: customer._id,
@@ -50,7 +50,9 @@ router.post('/', auth, async (req, res) => {
     .update('bartenders', { _id: cocktail.owner._id }, { 
       $inc: { raiting: 1 }})
     .run()
-    res.send(like);
+    .then(() => {
+      res.send(like);
+    });
   }
   catch(ex) {
     res.status(500).send('Exception: \n' + ex);
@@ -60,20 +62,19 @@ router.post('/', auth, async (req, res) => {
 //remove the like
 router.delete('/', auth, async (req, res) => {
   //authorize
-  if(req.user.role != 'customer') return res.status(401).send(`Only customers can access this resource`);
+  if(req.user.role != 'customer') return res.status(401).send({message: `Only customers can access this resource`});
   //set the customer
   const customer = await Customer.findById(req.user._id);
-  if(!customer) return res.status(400).send(`No customer user with this id`);
+  if(!customer) return res.status(404).send({message: `No customer user with this id`});
   //validate the body of the request
   const error = validateLike(req.body);
-  if(error) return res.status(400).send(error.details[0].message);
+  if(error) return res.status(400).send(error.details[0]);
   //get the cocktail
   const cocktail = await Cocktail.findById(req.body.cocktailId);
-  if(!cocktail) return res.status(400).send(`No cocktail with this id`);
+  if(!cocktail) return res.status(404).send({message: `No cocktail with this id`});
   //search for this cocktail in favorites list
-  if(customer.favCocktails.indexOf(cocktail._id) < 0) return res.status(400).send(`This cocktail is not in your favorite list`);
+  if(customer.favCocktails.indexOf(cocktail._id) < 0) return res.status(400).send({message: `This cocktail is not in your favorite list`});
   const like = await Like.findOne({ $and: [{customerId: customer._id}, {cocktailId: cocktail._id}]});
-  console.log(like)
   // delete the like and remove it from the cocktail list of likes
   try {
     new Fawn.Task()
@@ -87,7 +88,9 @@ router.delete('/', auth, async (req, res) => {
     .update('bartenders', { _id: cocktail.owner._id }, { 
       $inc: { raiting: -1 }})
     .run()
-    res.send(`Cocktail ${cocktail.name} is no longer in your favorite list`);
+    .then(() => {
+      res.send({message: `Cocktail ${cocktail.name} is no longer in your favorite list`});
+    });
   }
   catch(ex) {
     res.status(500).send('Exception: \n' + ex);
